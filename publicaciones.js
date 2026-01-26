13df65ea1e271c8b892ee1a123c1b8d96be3529d
tecnomago3000-coder <tecnomago3000@gmail.com>
2026-01-26 06:33:25 -0300

    publicaciones.js

--- publicaciones.js
+++ publicaciones.js
@@ -1,9 +1,7 @@
 export const VIDEOS = [
-  "https://youtu.be/9D0Ow4vQ2cg",
+  "https://youtu.be/9D0Ow4vQ2cg ",
   "https://youtu.be/0D6EQjLnWg8",
-  "https://youtu.be/ne0GJmPFU7U",
-  "https://youtu.be/CJ3Vrxzoz-4",
-  "https://youtu.be/Z76BHt4O1e4",
+  "https://youtu.be/ne0GJmPFU7U ",
+  "https://youtu.be/CJ3Vrxzoz-4 ",
   "https://youtu.be/aOzVHUC8IYM",
-  "https://youtu.be/cMypEvxMEKA"
 ];
